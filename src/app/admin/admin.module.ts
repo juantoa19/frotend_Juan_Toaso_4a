9b4e67e5d9659cee3d0fab78f7624daeaddc7e93
Jeremy339 <jal.pachoqyavirac.edu.ec>
2024-11-13 10:37:44 -0500

    Clase 12

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
+import { PerfilComponent } from './components/perfil/perfil.component';
+import { ClienteComponent } from './components/cliente/cliente.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PerfilComponent,
+    ClienteComponent
+  ],
   imports: [
     CommonModule,
     AdminRoutingModule
